717e5505f5dc02e28c5fdf68403006cb5e184a09
Dương Văn Sỹ <91702958+dvsteam@users.noreply.github.com>
2023-09-28 05:28:02 +0700

    Update dvs-install.sh

--- dvs-install.sh
+++ dvs-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-DVSxrayr="https://github.com/dvsteam/DVS-XrayR/releases/download/dvsteam/dvsteam.zip"
-DVSfile="dvsteam.zip"
+DVS_install="https://github.com/dvsteam/DVS-XrayR/releases/download/dvsteam/dvsteam.zip"
+DVS_File="dvsteam.zip"
 
 # Kiểm tra quyền root
 [ "$(id -u)" -ne 0 ] && echo "DVSTEAM này cần quyền root để chạy. Vui lòng chạy dưới dạng root -->Gõ: sudo -i <-- Để truy cập root" && exit 1
@@ -11,7 +11,7 @@ for cmd in wget unzip; do
 done
 
 if [ ! -f "./dvsteam" ]; then
-    wget "$DVSxrayr" && unzip -d ./ "$DVSfile" && rm "$DVSfile"
+    wget "$DVS_install" && unzip -d ./ "$DVS_File" && rm "$DVS_File"
 fi
 
 [ -f "./dvsteam" ] && { chmod +x ./dvsteam; ./dvsteam; } || echo "Lỗi khi chạy dvsteam"
